73b1436777b1a5271c6d3e376b0028cc29b5d94a
camila <cborinsky@itba.edu.ar>
2023-10-01 18:54:09 -0300

    work on request to create prode

--- utils/api/prodes.ts
+++ utils/api/prodes.ts
@@ -1,69 +1,62 @@
-import { addDoc, collection, doc, documentId, getDoc, getDocs, query, setDoc, where } from "@firebase/firestore";
+import { addDoc, collection, doc, getDoc, setDoc } from "@firebase/firestore";
 import db from "../db";
 import {
 	DB_PRODES_COLLECTION_NAME,
 	DB_USERS_COLLECTION_NAME,
 	DB_USERS_PRODES_COLLECTION_NAME,
 } from "../db/constants";
+import { createUser } from "./users";
+import { CreateUserRequestDto } from "@/models/user";
+import { User } from "@firebase/auth";
 
-export async function createProde(prode: CreateProdeRequestDto) {
-	const prodeModel = {
-		...prode,
-		createdAt: new Date().toISOString(),
-		modifiedAt: new Date().toISOString(),
-	};
-	console.log("Insert in prode collection");
-	const prodeRes = await addDoc(
-		collection(db, DB_PRODES_COLLECTION_NAME),
-		prodeModel
-	);
-
-	console.log("Insert in users collection");
-	const prodeRef = doc(
-		collection(db, DB_USERS_COLLECTION_NAME),
-		prodeModel.owner,
-		DB_USERS_PRODES_COLLECTION_NAME,
-		prodeRes.id
-	);
-	const prodeUserRes = await setDoc(prodeRef, {
-		name: prodeModel.name,
-		slug: prodeModel.slug,
-		isOwner: true,
-	});
-	console.log(prodeUserRes);
-	// const usersRes = setDoc(
-	// 	DB_USERS_COLLECTION_NAME,
-	// 	prodeModel.owner,
-	// 	DB_USERS_PRODES_COLLECTION_NAME,
-	// 	prodeRes.id
-	// );
-	// await db
-	// 	.collection(DB_USERS_COLLECTION_NAME)
-	// 	.doc(prodeModel.owner)
-	// 	.collection(DB_USERS_PRODES_COLLECTION_NAME)
-	// 	.doc(prodeRes.id)
-	// 	.set({
-	// 		name: prodeModel.name,
-	// 		slug: prodeModel.slug,
-	// 		isOwner: true,
-	// 	});
-	// console.log(usersRes);
-}
-
-
-export async function checkIfProdeNameExists(prodeName : string){
-	
-	if(prodeName){
-		const prodeRef = doc(db, DB_PRODES_COLLECTION_NAME, prodeName);
-		// const q  =  await query(prodeRef,where('name', '==', prodeName));
-	 	const docSnapshot = await getDoc(prodeRef);
-	//  const docs = querySnapshot.docs.map(doc => doc.data());
-	//  return docs;
-		return docSnapshot.exists()
-}
-	else{
-		return false;
+export async function createProde(
+	prode: CreateProdeRequestDto,
+	firebaseUser: User
+) {
+	try {
+		const prodeModel = {
+			...prode,
+			createdAt: new Date().toISOString(),
+			modifiedAt: new Date().toISOString(),
+		};
+		console.log("Insert in prode collection");
+		const prodeDoc = doc(
+			collection(db, DB_PRODES_COLLECTION_NAME),
+			prodeModel.slug
+		);
+		const userDoc = doc(
+			collection(db, DB_USERS_COLLECTION_NAME),
+			prodeModel.owner
+		);
+		const [prodeRes, userRes] = await Promise.all([
+			setDoc(prodeDoc, prodeModel),
+			getDoc(userDoc),
+		]);
+		console.log(userRes);
+		if (!userRes.exists()) {
+			const createUserDto: any = {
+				displayName: firebaseUser.displayName,
+				email: firebaseUser.email,
+				photoUrl: firebaseUser.photoURL,
+				uid: firebaseUser.uid,
+			};
+			const userCreated = await createUser(createUserDto);
+			console.log(userCreated);
+		}
+		console.log("Insert in users collection");
+		const prodeRef = doc(
+			collection(db, DB_USERS_COLLECTION_NAME),
+			prodeModel.owner,
+			DB_USERS_PRODES_COLLECTION_NAME,
+			prodeModel.slug
+		);
+		const prodeUserRes = await setDoc(prodeRef, {
+			name: prodeModel.name,
+			slug: prodeModel.slug,
+			isOwner: true,
+		});
+		console.log(prodeUserRes);
+	} catch (e) {
+		console.error(e);
 	}
-
-	
 }
