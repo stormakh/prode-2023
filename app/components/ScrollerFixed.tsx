865c558986bc9ef8f38f74b330ee5634e7372f2f
camila <cborinsky@itba.edu.ar>
2023-10-09 00:43:03 -0300

    allow for input votes to be custom inputs instead of only controlled ones

--- app/components/ScrollerFixed.tsx
+++ app/components/ScrollerFixed.tsx
@@ -1,111 +1,202 @@
-'use client'
+"use client";
 import { CSSProperties, Dispatch, SetStateAction, useState } from "react";
 
-
+const validateNumber = (number: number) => {
+	if (number < 0 || number >= 100) {
+		return false;
+	}
+	return true;
+};
 
 type voteDouble = {
-    big : number,
-    small : number,
-}
+	big: number;
+	small: number;
+};
 
 const enum numberPart {
-    big = "big",
-    small = "small",
+	big = "big",
+	small = "small",
 }
 
-export default function ScrollerFixed({theme, votes,candidateId, initialVotes} : {theme : CSSProperties, votes : (candidateId : number, voteValue : number)=> void, candidateId : number, initialVotes : number}) {
-    
-    
-   
-    const [currentSelected, setCurrentSelected] = useState<voteDouble>({big: initialVotes-(initialVotes%1), small: Math.round((initialVotes-Math.floor(initialVotes))*100)});
-    
-    const handlePlusButton = (setter : Dispatch<SetStateAction<voteDouble>>, numberPart : numberPart) => {
-        
-        var newVoteValue = currentSelected.big + currentSelected.small/100;
+export default function ScrollerFixed({
+	theme,
+	votes,
+	candidateId,
+	initialVotes,
+}: {
+	theme: CSSProperties;
+	votes: (candidateId: number, voteValue: number) => void;
+	candidateId: number;
+	initialVotes: number;
+}) {
+	const [currentSelected, setCurrentSelected] = useState<voteDouble>({
+		big: initialVotes - (initialVotes % 1),
+		small: Math.round((initialVotes - Math.floor(initialVotes)) * 100),
+	});
 
-        
-        
-        if(numberPart == "big"){
-           newVoteValue = newVoteValue + 1;
-        } else {
-            newVoteValue = newVoteValue + 0.01;
-        }
-        try {
-           
-            votes(candidateId, newVoteValue);
-            if (numberPart == "big") {
-                setter(prevState => ({...prevState, big: prevState.big + 1}));
-            }
-            else {
-                if(currentSelected.small == 99)
-                {
-                    console.log("small is 99");
-                    return;
-                }
-                setter(prevState => ({...prevState, small: prevState.small + 1}));
-            }
-        }catch(error)
-        {
-            console.log(error)
-        }
-        
+	const handlePlusButton = (
+		setter: Dispatch<SetStateAction<voteDouble>>,
+		numberPart: numberPart
+	) => {
+		var newVoteValue = currentSelected.big + currentSelected.small / 100;
 
-    }
+		if (numberPart == "big") {
+			newVoteValue = newVoteValue + 1;
+		} else {
+			newVoteValue = newVoteValue + 0.01;
+		}
+		try {
+			votes(candidateId, newVoteValue);
+			if (numberPart == "big") {
+				setter((prevState) => ({
+					...prevState,
+					big: prevState.big + 1,
+				}));
+			} else {
+				if (currentSelected.small == 99) {
+					console.log("small is 99");
+					return;
+				}
+				setter((prevState) => ({
+					...prevState,
+					small: prevState.small + 1,
+				}));
+			}
+		} catch (error) {
+			console.log(error);
+		}
+	};
 
-    const handleMinusButton =  (setter : Dispatch<SetStateAction<voteDouble>>, numberPart : numberPart ) => {
-    
-        if(currentSelected.small-1 < 0 && numberPart == "small"){
-            return;
-        }
-        if(currentSelected.big == 0 && numberPart == "big"){
-            return;
-        }
-        var newVoteValue = currentSelected.big + currentSelected.small/100;
-        if(numberPart == "big"){
-           newVoteValue = newVoteValue - 1;
-        } else {
-            newVoteValue = newVoteValue - 0.01;
-        }
-        try {
-            votes(candidateId, newVoteValue);
-        }catch(error)
-        {
-            console.log(error);
-        }
-        if (numberPart == "big") {
-            setter(prevState => ({...prevState, big: prevState.big - 1}));
-        }
-        else {
-            setter(prevState => ({...prevState, small: prevState.small - 1}));
-        }
-    }
+	const handleMinusButton = (
+		setter: Dispatch<SetStateAction<voteDouble>>,
+		numberPart: numberPart
+	) => {
+		if (currentSelected.small - 1 < 0 && numberPart == "small") {
+			return;
+		}
+		if (currentSelected.big == 0 && numberPart == "big") {
+			return;
+		}
+		var newVoteValue = currentSelected.big + currentSelected.small / 100;
+		if (numberPart == "big") {
+			newVoteValue = newVoteValue - 1;
+		} else {
+			newVoteValue = newVoteValue - 0.01;
+		}
+		try {
+			votes(candidateId, newVoteValue);
+		} catch (error) {
+			console.log(error);
+		}
+		if (numberPart == "big") {
+			setter((prevState) => ({ ...prevState, big: prevState.big - 1 }));
+		} else {
+			setter((prevState) => ({
+				...prevState,
+				small: prevState.small - 1,
+			}));
+		}
+	};
+	const updateParent = (newVote: voteDouble) => {
+		var newVoteValue = newVote.big + newVote.small / 100;
+		console.log(newVote);
+		votes(candidateId, newVoteValue);
+	};
 
-    function MinusButton({setter, numberPart} : { setter : Dispatch<SetStateAction<voteDouble>>,numberPart : numberPart}){
-        return(
-            <button className="px-8  border-2 rounded-md w-full " onClick={e => handleMinusButton(setter, numberPart) } style={{borderColor : theme.color}}>-</button>
-        )
-    }
-    function PlusButton({ setter, numberPart} : { setter : Dispatch<SetStateAction<voteDouble>>, numberPart : numberPart}){
-        return(
-            <button className="px-8  border-2 rounded-md w-full " onClick={e => handlePlusButton(setter, numberPart)} style={{borderColor : theme.color}}>+</button>
-        )
-    }
+	function MinusButton({
+		setter,
+		numberPart,
+	}: {
+		setter: Dispatch<SetStateAction<voteDouble>>;
+		numberPart: numberPart;
+	}) {
+		return (
+			<button
+				className="px-8  border-2 rounded-md w-full "
+				onClick={(e) => handleMinusButton(setter, numberPart)}
+				style={{ borderColor: theme.color }}
+			>
+				-
+			</button>
+		);
+	}
+	function PlusButton({
+		setter,
+		numberPart,
+	}: {
+		setter: Dispatch<SetStateAction<voteDouble>>;
+		numberPart: numberPart;
+	}) {
+		return (
+			<button
+				className="px-8  border-2 rounded-md w-full "
+				onClick={(e) => handlePlusButton(setter, numberPart)}
+				style={{ borderColor: theme.color }}
+			>
+				+
+			</button>
+		);
+	}
 
-    return (
-        <div className="flex flex-grow justify-center items-center text-xl" style={{...theme}}>
-            <div className="p-1 flex-grow">
-            <PlusButton  setter={setCurrentSelected} numberPart={numberPart.big}/>
-            <p className="p-3 text-3xl">{currentSelected.big}</p>
-            <MinusButton  setter={setCurrentSelected} numberPart={numberPart.big}/>
-            </div>
-            <div className="">
-            <p className="m-0 text-3xl">,</p>
-            </div>
-            <div className="p-1 flex-grow">
-            <PlusButton setter={setCurrentSelected} numberPart={numberPart.small}/>
-            <p  className="p-3 text-3xl">{currentSelected.small-(currentSelected.small%1)}</p>
-            <MinusButton setter={setCurrentSelected} numberPart={numberPart.small}/>
-            </div>
-        </div>
-    )
+	return (
+		<div
+			className="flex flex-grow justify-center items-center text-xl"
+			style={{ ...theme }}
+		>
+			<div className="p-1 flex-grow">
+				<PlusButton
+					setter={setCurrentSelected}
+					numberPart={numberPart.big}
+				/>
+				<input
+					className="p-3 text-3xl w-8/12 focus:border-t-transparent focus:outline-0"
+					value={currentSelected.big}
+					onChange={(e) => {
+						const newNumber = Number(e.target.value);
+						if (validateNumber(newNumber)) {
+							const newValue: voteDouble = {
+								...currentSelected,
+								big: newNumber,
+							};
+							setCurrentSelected(newValue);
+							updateParent(newValue);
+						}
+					}}
+				/>
+				<MinusButton
+					setter={setCurrentSelected}
+					numberPart={numberPart.big}
+				/>
+			</div>
+			<div className="">
+				<p className="m-0 text-3xl">,</p>
+			</div>
+			<div className="p-1 flex-grow">
+				<PlusButton
+					setter={setCurrentSelected}
+					numberPart={numberPart.small}
+				/>
+				<input
+					className="p-3 text-3xl w-8/12 focus:border-t-transparent focus:outline-0"
+					value={currentSelected.small - (currentSelected.small % 1)}
+					onChange={(e) => {
+						const newNumber = Number(e.target.value);
+						if (validateNumber(newNumber)) {
+							const newValue: voteDouble = {
+								...currentSelected,
+								small: newNumber,
+							};
+							setCurrentSelected(newValue);
+							updateParent(newValue);
+						}
+					}}
+				/>
+
+				<MinusButton
+					setter={setCurrentSelected}
+					numberPart={numberPart.small}
+				/>
+			</div>
+		</div>
+	);
 }
--- app/prode/[slug]/input_vote.tsx
+++ app/prode/[slug]/input_vote.tsx
@@ -1,208 +1,215 @@
-"use client"
-import { useEffect, useMemo, useState } from "react"
-import { CandidateList } from "@/utils/candidateInfo/candidateList"
-import CandidateVoteBox from "../../components/CandidateVoteBox"
-import Navbar from "../../components/Navbar"
-import { useAuth } from "../../contexts/AuthContext"
-import { createVote, getVote } from "@/utils/api/votes"
-import NoUserModal from "@/app/components/NoUserModal"
+"use client";
+import { useEffect, useMemo, useState } from "react";
+import { CandidateList } from "@/utils/candidateInfo/candidateList";
+import CandidateVoteBox from "../../components/CandidateVoteBox";
+import Navbar from "../../components/Navbar";
+import { useAuth } from "../../contexts/AuthContext";
+import { createVote, getVote } from "@/utils/api/votes";
+import NoUserModal from "@/app/components/NoUserModal";
 
 type VoteType = {
-  candidateId: number
-  voteValue: number
-}
+	candidateId: number;
+	voteValue: number;
+};
 
 const initialCandidateVote: VoteType[] = [
-  {
-    candidateId: 1,
-    voteValue: 0.0,
-  },
-  {
-    candidateId: 2,
-    voteValue: 0.0,
-  },
-  {
-    candidateId: 3,
-    voteValue: 0.0,
-  },
-  {
-    candidateId: 4,
-    voteValue: 0.0,
-  },
-  {
-    candidateId: 5,
-    voteValue: 0.0,
-  },
-]
+	{
+		candidateId: 1,
+		voteValue: 0.0,
+	},
+	{
+		candidateId: 2,
+		voteValue: 0.0,
+	},
+	{
+		candidateId: 3,
+		voteValue: 0.0,
+	},
+	{
+		candidateId: 4,
+		voteValue: 0.0,
+	},
+	{
+		candidateId: 5,
+		voteValue: 0.0,
+	},
+];
 
 initialCandidateVote.forEach((candidate, index) => {
-  candidate.voteValue = CandidateList[index].initialVotes
-})
+	candidate.voteValue = CandidateList[index].initialVotes;
+});
 
 export default function InputVote({
-  params,
+	params,
 }: {
-  params: { slug: string; setShowProdeStats: (shouldShow: boolean) => void ; ownerName : string | undefined}
+	params: {
+		slug: string;
+		setShowProdeStats: (shouldShow: boolean) => void;
+		ownerName: string | undefined;
+	};
 }) {
-  const [vote, setVote] = useState<VoteType[]>(initialCandidateVote)
-  const [errorMessage, setErrorMessage] = useState<string>("")
-  const { firebaseUser,AnonUsername } = useAuth()
+	const [vote, setVote] = useState<VoteType[]>(initialCandidateVote);
+	const [errorMessage, setErrorMessage] = useState<string>("");
+	const { firebaseUser, AnonUsername } = useAuth();
 
-  useEffect(() => {
-    console.log("VOTING SLUG:", params.slug)
-    console.log("VOTING USER:", firebaseUser && firebaseUser.uid)
-    console.log("TODO ALERT: CHECK IF PRODE EXISTS & IF USER HAS VOTED")
-    const getVoteAsync = async () => {
-      if (!firebaseUser) return
-      const vote = await getVote(params.slug, firebaseUser.uid)
-      console.log("INITIAL VOTE CHECK:", vote)
-    }
-    getVoteAsync()
-  }, [firebaseUser])
+	useEffect(() => {
+		console.log("VOTING SLUG:", params.slug);
+		console.log("VOTING USER:", firebaseUser && firebaseUser.uid);
+		console.log("TODO ALERT: CHECK IF PRODE EXISTS & IF USER HAS VOTED");
+		const getVoteAsync = async () => {
+			if (!firebaseUser) return;
+			const vote = await getVote(params.slug, firebaseUser.uid);
+			console.log("INITIAL VOTE CHECK:", vote);
+		};
+		getVoteAsync();
+	}, [firebaseUser]);
 
-  function handleVote(candidateId: number, voteValue: number) {
-    var res = 0
-    vote.forEach((candidate) => {
-      // sumo los votos de todos los candidatos
-      if (candidate.candidateId !== candidateId) {
-        res = res + candidate.voteValue
-      }
-    })
+	function handleVote(candidateId: number, voteValue: number) {
+		var res = 0;
+		vote.forEach((candidate) => {
+			// sumo los votos de todos los candidatos
+			if (candidate.candidateId !== candidateId) {
+				res = res + candidate.voteValue;
+			}
+		});
 
-    res = res + voteValue // sumo el voto que se quiere agregar
+		res = res + voteValue; // sumo el voto que se quiere agregar
 
-    if (res <= 100 && res >= 0) {
-      // verifico que no se pase de 100 o a los negativos
-      const nextVotes = vote.map((candidate) => {
-        if (candidate.candidateId === candidateId) {
-          return {
-            ...candidate,
-            voteValue: voteValue,
-          }
-        } else {
-          return candidate
-        }
-      })
+		// if (res <= 100 && res >= 0) {
+		// verifico que no se pase de 100 o a los negativos
+		const nextVotes = vote.map((candidate) => {
+			if (candidate.candidateId === candidateId) {
+				return {
+					...candidate,
+					voteValue: voteValue,
+				};
+			} else {
+				return candidate;
+			}
+		});
 
-      setVote(nextVotes)
-    } else {
-      console.log("no se puede votar")
-      if (res > 100) {
-        throw new Error("Te pasaste por" + (res - 100) + "%")
-      }
-      if (res < 0) {
-        throw new Error("los votos no pueden ser negativos")
-      }
-    }
-  }
+		setVote(nextVotes);
+		// } else {
+		// 	console.log("no se puede votar");
+		// 	if (res > 100) {
+		// 		throw new Error("Te pasaste por" + (res - 100) + "%");
+		// 	}
+		// 	if (res < 0) {
+		// 		throw new Error("los votos no pueden ser negativos");
+		// 	}
+		// }
+	}
 
-    function handleErrorModal(){
-      if(errorMessage == 'Debes registrarte para votar') {
-        console.log("ERROR MODAL")
-        return true
-      }
-      else {
-        return false
-      } 
-    }
- 
-    function endModal(){
-      setErrorMessage("")
-    }
-  
+	function handleErrorModal() {
+		if (errorMessage == "Debes registrarte para votar") {
+			console.log("ERROR MODAL");
+			return true;
+		} else {
+			return false;
+		}
+	}
 
-  async function handleClickUploadResults() {
-    if(AnonUsername == null){
-      setErrorMessage("Debes registrarte para votar")
-      return;
-    }
-    if (totalVotes !== '100.00') {
-      setErrorMessage("Tus votos NO suman 100%")
-      return;
-    }
-    setErrorMessage("")
+	function endModal() {
+		setErrorMessage("");
+	}
 
-    const votes: Record<string, number> = {}
-    CandidateList.forEach((candidate, index) => {
-      votes[candidate.candidateIdentifier as keyof typeof votes] =
-        vote[index].voteValue
-    })
-    console.log("FINAL VOTES:", votes)
-    await createVote(
-      {
-        voterUid: firebaseUser.uid,
-        votes: votes as CandidateVotes,
-      },
-      params.slug
-    ).then(() => {
-      params.setShowProdeStats(true)
-    })
-  }
+	async function handleClickUploadResults() {
+		if (totalVotes !== "100.00") {
+			setErrorMessage("Tus votos NO suman 100%");
+			return;
+		}
+		if (AnonUsername == null) {
+			setErrorMessage("Debes registrarte para votar");
+			return;
+		}
 
-  const totalVotes = useMemo(() => {
-    var res = 0
-    vote.forEach((candidate) => {
-      res = res + candidate.voteValue
-    })
-    return res.toFixed(2)
-  }, [vote])
+		setErrorMessage("");
 
-  return (
-    <main className="">
-      <Navbar isEnabledCreaElTuyoBtn={firebaseUser.isAnonymous}/>
-      {handleErrorModal() ? <NoUserModal endModal={endModal}/> : null}
-      <div className="flex justify-center flex-col items-center p-2 ">
-        <div className="text-teal-500 max-w-fit flex flex-col">
-          <h2 className="text-2xl font-bold">
-            {params.ownerName != null && params.ownerName != undefined ? params.ownerName + " te intivó al Prode!" : params.slug}
-          </h2>
-          <p className="font-bold p-1">
-            Elegi los porcentajes que crees que cada uno de los candidatos va a
-            sacar en las elecciones para presidente del 2023.
-          </p>
-          <p className="font-bold border-b-1/2 p-2  border-teal-500">
-            Juga y comparti con tus amigos!
-          </p>
+		const votes: Record<string, number> = {};
+		CandidateList.forEach((candidate, index) => {
+			votes[candidate.candidateIdentifier as keyof typeof votes] =
+				vote[index].voteValue;
+		});
+		console.log("FINAL VOTES:", votes);
+		await createVote(
+			{
+				voterUid: firebaseUser.uid,
+				votes: votes as CandidateVotes,
+			},
+			params.slug
+		).then(() => {
+			params.setShowProdeStats(true);
+		});
+	}
 
-          <div className="flex flex-col md:flex-wrap md:flex-row gap-4">
-            {CandidateList.map((candidate, index) => {
-              return (
-                <CandidateVoteBox
-                  key={index}
-                  CandidateName={candidate.candidateName}
-                  ImageUrl={candidate.imageUrl}
-                  theme={candidate.theme}
-                  candidateId={vote[index].candidateId}
-                  votes={handleVote}
-                  initialVotes={candidate.initialVotes}
-                />
-              )
-            })}
-          </div>
-        </div>
-      </div>
+	const totalVotes = useMemo(() => {
+		var res = 0;
+		vote.forEach((candidate) => {
+			res = res + candidate.voteValue;
+		});
+		return res.toFixed(2);
+	}, [vote]);
 
-      <div className="sticky bottom-0   w-full rounded-none  justify-center items-center font-bold text-teal-500 bg-juan p-2 mt-24">
-        <p>{"Tus votos totales son :" + totalVotes + "%"}</p>
-      </div>
-      <div className="text-center line-clamp-2 text-red-500 text-xl font-bold p-1">
-        {errorMessage}
-      </div>
-      <div className="flex w-full  justify-center items-center font-bold text-white p-2 mt-24">
-        <button
-          className="p-2 rounded-md bg-teal-500 text-xl"
-          onClick={handleClickUploadResults}
-        >
-          Ver Resultados
-        </button>
-      </div>
-      <div className="px-2 my-5 ">
-        <p className="text-center text-xs text-gray-400">
-          Al tocar este boton estas compartiendo tus predicciones con el resto
-          de los integrantes del grupo. Tus predicciones van a poder ser
-          modificadas tras el transcurso de una semana.
-        </p>
-      </div>
-    </main>
-  )
+	return (
+		<main className="">
+			<Navbar isEnabledCreaElTuyoBtn={!firebaseUser?.isAnonymous} />
+			{handleErrorModal() ? <NoUserModal endModal={endModal} /> : null}
+			<div className="flex justify-center flex-col items-center p-2 ">
+				<div className="text-teal-500 max-w-fit flex flex-col">
+					<h2 className="text-2xl font-bold">
+						{params.ownerName != null &&
+						params.ownerName != undefined
+							? params.ownerName + " te intivó al Prode!"
+							: params.slug}
+					</h2>
+					<p className="font-bold p-1">
+						Elegi los porcentajes que crees que cada uno de los
+						candidatos va a sacar en las elecciones para presidente
+						del 2023.
+					</p>
+					<p className="font-bold border-b-1/2 p-2  border-teal-500">
+						Juga y comparti con tus amigos!
+					</p>
+
+					<div className="flex flex-col md:flex-wrap md:flex-row gap-4">
+						{CandidateList.map((candidate, index) => {
+							return (
+								<CandidateVoteBox
+									key={index}
+									CandidateName={candidate.candidateName}
+									ImageUrl={candidate.imageUrl}
+									theme={candidate.theme}
+									candidateId={vote[index].candidateId}
+									votes={handleVote}
+									initialVotes={candidate.initialVotes}
+								/>
+							);
+						})}
+					</div>
+				</div>
+			</div>
+
+			<div className="sticky bottom-0   w-full rounded-none  justify-center items-center font-bold text-teal-500 bg-juan p-2 mt-24">
+				<p>{"Tus votos totales son :" + totalVotes + "%"}</p>
+			</div>
+			<div className="text-center line-clamp-2 text-red-500 text-xl font-bold p-1">
+				{errorMessage}
+			</div>
+			<div className="flex w-full  justify-center items-center font-bold text-white p-2 mt-24">
+				<button
+					className="p-2 rounded-md bg-teal-500 text-xl"
+					onClick={handleClickUploadResults}
+				>
+					Ver Resultados
+				</button>
+			</div>
+			<div className="px-2 my-5 ">
+				<p className="text-center text-xs text-gray-400">
+					Al tocar este boton estas compartiendo tus predicciones con
+					el resto de los integrantes del grupo. Tus predicciones van
+					a poder ser modificadas tras el transcurso de una semana.
+				</p>
+			</div>
+		</main>
+	);
 }
