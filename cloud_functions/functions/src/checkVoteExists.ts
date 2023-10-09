d649cfd6d3bfe2f8d190671778a0d45346c711ed
atormakh <atormakh@itba.edu.ar>
2023-10-09 12:16:28 -0300

    feat(back): create checkIfVoteExists function

--- cloud_functions/functions/src/checkVoteExists.ts
+++ cloud_functions/functions/src/checkVoteExists.ts
@@ -1,30 +1,38 @@
 import * as admin from "firebase-admin"
 import * as functions from "firebase-functions"
+import * as cors from "cors"
 
+const corsHandler = cors({ origin: true })
 export const checkVoteExists = functions.https.onRequest(
-  async (request, response) => {
-    try {
-      const prodeSlug = request.query.prode_slug as string
-      const voteId = request.query.vote_id as string
+  (request, response) => {
+    corsHandler(request, response, async () => {
+      try {
+        try {
+          const prodeSlug = request.query.prode_slug as string
+          const voteId = request.query.vote_id as string
 
-      // Verifica que prodeSlug y voteId estén presentes en la solicitud
-      if (!prodeSlug || !voteId) {
-        response
-          .status(400)
-          .send({ error: "Prode slug and vote ID are required." })
-        return
+          // Verifica que prodeSlug y voteId estén presentes en la solicitud
+          if (!prodeSlug || !voteId) {
+            response
+              .status(400)
+              .send({ error: "Prode slug and vote ID are required." })
+            return
+          }
+
+          // Referencia al documento
+          const docRef = admin
+            .firestore()
+            .doc(`prodes/${prodeSlug}/votes/${voteId}`)
+          const doc = await docRef.get()
+
+          // Si el documento existe, devuelve true, de lo contrario false
+          response.send({ exists: doc.exists })
+        } catch (error) {
+          response.status(500).send({ error: "Internal server error." })
+        }
+      } catch (error) {
+        response.status(500).send({ error: "Cors error." })
       }
-
-      // Referencia al documento
-      const docRef = admin
-        .firestore()
-        .doc(`prodes/${prodeSlug}/votes/${voteId}`)
-      const doc = await docRef.get()
-
-      // Si el documento existe, devuelve true, de lo contrario false
-      response.send({ exists: doc.exists })
-    } catch (error) {
-      response.status(500).send({ error: "Internal server error." })
-    }
+    })
   }
 )
